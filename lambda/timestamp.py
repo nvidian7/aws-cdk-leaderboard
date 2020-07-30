d538e2911544feea42bed2023d988d9d0bd2ae8e
nvidian7 <woojun.shim@gmail.com>
2020-07-30 21:38:54 +0900

    하드코딩된 값을 max_timestamp 상수로 변경 timestamp 관련 주석 추가

--- lambda/timestamp.py
+++ lambda/timestamp.py
@@ -1,6 +1,9 @@
 import time
 import datetime as pydatetime
 
+# 06/11/2128 @ 8:53am (UTC)
+max_timestamp = 5000000000
+
 
 # get datetime
 def get_now():
@@ -9,8 +12,11 @@ def get_now():
 
 # 10 digit timestamp
 def get_now_timestamp():
-    return int(get_now().timestamp())
+    return get_now().timestamp()
 
 
+# redis의 sortedset value 사전순 정렬을 내림차순으로 유지하기
+# 위해서는 timestamp 크기 비교 결과를 반대로 뒤집는 처리가 필요하고,
+# 이를 위해서는 충분히 큰 timestamp 값에서 현재 timestamp를 빼서 그 차이를 이용
 def get_reverse_timestamp():
-    return int(5000000000-get_now_timestamp())
+    return int(max_timestamp-get_now_timestamp())
